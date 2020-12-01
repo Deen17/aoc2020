0b0d20631200bb77b4bacf98cfab4bb2bdee62cc
Adil Desktop <adilriazudeen@gmail.com>
2020-12-01 02:04:24 -0600

    first day part 2

--- src/day01.rs
+++ src/day01.rs
@@ -17,3 +17,34 @@ pub fn part1(inp: String) {
         }
     }
 }
+
+pub fn part2(inp: String){
+    let mut sorted: Vec<u32> = 
+        inp
+            .split_ascii_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+    sorted.sort();
+
+    let total: u32 = 2020;
+    
+    for (i,first) in (&sorted).iter().enumerate(){
+        let desired_num = total - first;
+        let (_left,right) = sorted.split_at(i+1);
+
+        for second in (right).iter(){
+            
+            if first + second + second > total{
+                continue;
+            }
+            let third = desired_num - second;
+
+            if let Ok(_pos) = right.binary_search(&third){
+                println!("{}, {}, {}", first, second, third);
+                println!("{}", first * second * third);
+                return;
+            }
+        }
+
+    }
+}
